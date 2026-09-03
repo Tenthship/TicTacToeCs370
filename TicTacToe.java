33c802a0b2f423ee3a647505f94b244eb125050e
George Cohen <georgercohen@gmail.com>
2026-09-02 18:02:57 -0700

    Structure

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,32 @@
 public class TicTacToe {
-    
+    private int BOARDSIZE = 3;
+    private enum Status {WIN, DRAW, CONTINUE};
+    private char board[][];
+    private boolean firstPlayer;
+    private boolean gameOver;
+
+    public void play() {
+
+    }
+
+    private void printStatus() {
+
+    }
+
+    private Status gameStatus() {
+        return Status.CONTINUE;
+    }
+
+    public void printBoard() {
+
+    }
+
+    private void printSymbol(int col, char value) {
+
+    }
+
+    private boolean validMove(int row, int col) {
+        return false;
+    }
+
 }
